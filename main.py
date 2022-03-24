85d1d990615c64786260a23200ce1d9fb3bd6fe8
AnthonyRamirezNC <thearamirez2020@gmail.com>
2022-03-23 20:00:43 -0400

    Added Color to text corresponding to correct character

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import curses
 from curses import wrapper
 
+#starting screen function
 def start_screen(stdscr):
     stdscr.clear()
     stdscr.addstr("Welcome to Speed Typing Test!")
@@ -8,7 +9,40 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+def display_text(stdscr, target, current, wpm =0):
+    stdscr.addstr(target)
 
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+        stdscr.addstr(0, i, char, color)
+
+#WPM Function
+def wpm_test(stdscr): 
+    target_text = "Hello world this is some test text for this app!"
+    current_text = []
+    
+
+    while True:
+        stdscr.clear()
+        display_text(stdscr, target_text, current_text)    
+        stdscr.refresh()
+
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(key)
+
+
+        
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -16,6 +50,8 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
+
+    wpm_test(stdscr)
 
 
    
